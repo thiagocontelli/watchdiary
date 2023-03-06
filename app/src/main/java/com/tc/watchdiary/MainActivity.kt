0a093f746c3dafb6c95fa78f0b25a29fd896caf8
thiagocontelli <thiagocontellid@gmail.com>
2023-03-06 15:01:50 -0300

    feat: set up navigation

--- app/src/main/java/com/tc/watchdiary/MainActivity.kt
+++ app/src/main/java/com/tc/watchdiary/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.tc.watchdiary
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navController = Navigation.findNavController(this, R.id.host_fragment)
+
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
     }
 }
